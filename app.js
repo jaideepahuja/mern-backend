8abd21a10317b5437c2cd2fcc984a7ff2d7d85df
jaideepahuja <jaideepahuja05@gmail.com>
2026-01-14 12:17:12 +0530

    done

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const cors = require('cors'); // <--- 1. Import cors
 const app = express();
-app.get('/', (req, res) => res.send('Home Page'));
-app.get('/about', (req, res) => res.send('About Page'));
-app.listen(3000, () => console.log('Server running on port 3000'));
+
+app.use(cors()); // <--- 2. Use it (Allows everyone to connect)
+
+app.get('/', (req, res) => {
+    res.send('Home Page');
+});
+
+// ... rest of your routes ...
+
+app.listen(3000, () => console.log('Server running...'));
